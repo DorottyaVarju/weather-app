f236f555c52e1194b673b01b002da545ad456ac0
Varju Dorottya <varjudorottya@gmail.com>
2025-05-30 13:25:30 +0200

    chartdata fix

--- src/App.jsx
+++ src/App.jsx
@@ -83,20 +83,17 @@ const App = () => {
             <LineChart data={weatherService.getDataForChart(dates[0], chartData)} date={dates[0].split(', ').slice(0, 2).join(', ')} />
             <hr className="main-hr" />
             <h2>Forecast For The Next Few Days</h2>
-            <ScrollFade weather={dailyForecastItem[0]} date={dates[1]} isForecast={true} city={search} />
+            <ScrollFade weather={dailyForecastItem[1]} date={dates[1]} isForecast={true} city={search} />
             <LineChart data={weatherService.getDataForChart(dates[1], chartData)} date={dates[1]} />
             <hr />
-            <ScrollFade weather={dailyForecastItem[1]} date={dates[2]} isForecast={true} city={search} />
+            <ScrollFade weather={dailyForecastItem[2]} date={dates[2]} isForecast={true} city={search} />
             <LineChart data={weatherService.getDataForChart(dates[2], chartData)} date={dates[2]} />
             <hr />
-            <ScrollFade weather={dailyForecastItem[2]} date={dates[3]} isForecast={true} city={search} />
+            <ScrollFade weather={dailyForecastItem[3]} date={dates[3]} isForecast={true} city={search} />
             <LineChart data={weatherService.getDataForChart(dates[3], chartData)} date={dates[3]} />
             <hr />
-            <ScrollFade weather={dailyForecastItem[3]} date={dates[4]} isForecast={true} city={search} />
+            <ScrollFade weather={dailyForecastItem[4]} date={dates[4]} isForecast={true} city={search} />
             <LineChart data={weatherService.getDataForChart(dates[4], chartData)} date={dates[4]} />
-            <hr />
-            <ScrollFade weather={dailyForecastItem[4]} date={dates[5]} isForecast={true} city={search} />
-            <LineChart data={weatherService.getDataForChart(dates[5], chartData)} date={dates[5]} />
           </>
         )}
       </main>
